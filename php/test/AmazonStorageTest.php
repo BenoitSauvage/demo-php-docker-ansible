3a1747e8939bc46b4b4d84c6840c86b3b6c4dedc
Benoît Sauvage <benoit.sauvage44@gmail.com>
2020-06-10 14:59:50 -0400

    Demo PHP : Added comments

--- php/test/AmazonStorageTest.php
+++ php/test/AmazonStorageTest.php
@@ -13,27 +13,33 @@ class AmazonStorageTest extends TestCase
 
     protected function setUp(): void
     {
+        // Create response model for S3 putObject method
         $putModel = new Model([
             'ObjectURL' => 'http://s3-url.com/test.txt'
         ]);
 
+        // Create response model for S3 getObject method
         $getModel = new Model([
             'Body' => 'This is a random test file.'
         ]);
 
+        // Mock S3 client
         $client = $this->getMockBuilder('Aws\S3\S3Client')
             ->disableOriginalConstructor()
             ->addMethods(['putObject', 'getObject'])
             ->getMock();
 
+        // Mock S3->putObject method
         $client->method('putObject')
             ->with(['Bucket' => 'my-bucket', 'Key' => 'test.txt'])
             ->will($this->returnValue($putModel));
 
+        // Mock S3->getObject method
         $client->method('getObject')
             ->with(['Bucket' => 'my-bucket', 'Key' => 'test.txt'])
             ->will($this->returnValue($getModel));
 
+        // Create new instance of AmazonStorage using the S3 client mock
         $this->storage = new AmazonStorage($client);
     }
 
